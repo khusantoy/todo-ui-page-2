95592c6022afd3f7f73e5274500a209c4eee220c
khusantoy <tursunovkhusanboy@gmail.com>
2024-04-26 08:01:10 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,247 +9,263 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.grey[200],
-          appBar: AppBar(
-            title: Text("My Todo"),
-            centerTitle: true,
-            actions: const [
-              Icon(Icons.notifications),
-              SizedBox(
-                width: 25,
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: const Text(
+            "My Todo",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          centerTitle: true,
+          actions: const [
+            Icon(Icons.notifications),
+            SizedBox(
+              width: 25,
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 350,
+                height: 110,
+                decoration: BoxDecoration(
+                    color: Colors.green[100],
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.green,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Remaining Tasks (24)",
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 350,
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 350,
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 350,
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 350,
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 350,
+                height: 80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)),
+                child: const Row(
+                  children: [
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      size: 30,
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Complete Flutter UI App\nchallenge and upload it\non GitHub",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "1h 25m",
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {},
+                child: const Text(
+                  "+",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
               )
             ],
           ),
-          body: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 350,
-                  height: 110,
-                  decoration: BoxDecoration(
-                      color: Colors.green[100],
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.green,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      SizedBox(
-                        width: 30,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey, fontSize: 14),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "Remaining Tasks (24)",
-                  style: TextStyle(fontSize: 20),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: 350,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 350,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 350,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 350,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 350,
-                  height: 80,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: const Row(
-                    children: [
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Icon(
-                        Icons.check_circle,
-                        size: 30,
-                        color: Colors.blue,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Complete Flutter UI App\nchallenge and upload it\non GitHub",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "1h 25m",
-                        style: TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )),
+        ),
+        drawer: const Drawer(),
+      ),
     );
   }
 }
